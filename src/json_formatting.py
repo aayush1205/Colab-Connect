82f51989918dd7d8a7fdf517d6203b864a0b85e8
Aradhya-Tripathi <67282231+Aradhya-Tripathi@users.noreply.github.com>
2020-09-16 22:28:21 +0530

    Add files via upload

---
+++ src/json_formatting.py
@@ -0,0 +1,28 @@
+import json
+import os
+x = os.getcwd()
+x = x.split('/')
+root = x[1]
+name = x[2]
+os.chdir('/'+root+'/'+name+'/.config/Code/User')
+file = open('keybindings.json','r').read()
+json_file = json.loads(file)
+keys = []
+try:
+    for i in range(len(json_file)):
+        flag = True
+        keys.append(json_file[i]['key'])
+except Exception as e:
+    for i in (json_file.keys()):
+        keys.append(i)
+
+print(keys)
+def checkIfDuplicates(keys):
+    ''' Check if given list contains any duplicates '''
+    if len(keys) == len(set(keys)):
+        return False
+    else:
+        return True
+duplicates = checkIfDuplicates(keys)
+if duplicates:
+    
